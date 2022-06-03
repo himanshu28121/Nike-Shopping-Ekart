23bc3b774d2e29ccda19fa3e2f5fabb91b4e2b21
himanshu28121 <82179000+himanshu28121@users.noreply.github.com>
2022-06-03 17:36:52 +0530

    First Commit

---
+++ Ekart-Nike/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+    {
+      id: "ABC1",
+      name: "Nike Air Force 1 Shadow",
+      price: 120,
+      desc: "Multicolour Shoe, Featured in Editorial Fashion",
+      img: "images/Image1.jpg",
+    },
+    {
+      id: "ABC2",
+      name: "Nike Air Jorden 1",
+      price: 189,
+      desc: "Black and white with little touch of orange, Exhibited in Zurich",
+      img: "images/Image2.jpg",
+    },
+    {
+      id: "ABC3",
+      name: "Floating Nike Air Jorden 1 Mid",
+      price: 169,
+      desc: "Imperial Look with flying emperor, Exhibited in Amsterdam",
+      img:"images/Image3.jpg",
+    },
+    {
+      id: "ABC4",
+      name: "Nike SuperRep GO",
+      price: 119,
+      desc: "For Absolute Running performance, SuperRep Edition",
+      img:"images/Image4.jpg",
+    },
+  ];
